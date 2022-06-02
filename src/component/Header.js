2e6ab9e532f06e069808fec671073fdc63b90f94
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-06-01 23:44:52 -0700

    updated 06-01

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
     <div class="header">
-      <h1>Chania</h1>
+      <div className='HeaderButtonFrame'>
+        <div className='HeaderButtons'>
+          <Link to="/">
+                 <h5 className='HeaderButtonText'>Home</h5>
+          </Link>
+         </div>
+       </div>
      </div>
+     
 )
 
 export default Header;
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import '../App.css';
+import twitter from '../images/twitter.png';
+import linkedin from '../images/linkedin.png'
+import github from '../images/github.png'
 
 const Main = () => (
     <div class="row">
       <div class="col-3 col-s-3 menu">
         <ul>
-          <li>The Flight</li>
-          <li>The City</li>
-          <li>The Island</li>
-          <li>The Food</li>
+        <li>
+            <a href="https://twitter.com/BiteChacha" target="_blank">
+              <img className="img-responsive" src={twitter} alt="logo"/>
+	           </a>
+           </li>
+          <li>
+	        <a href="https://www.linkedin.com/in/ichiro-yamasaki-1692aa168/" target="_blank">
+            <img className="img-responsive" src={linkedin} alt="logo"/>
+	         </a>
+           </li>
+          <li>
+	        <a href="https://github.com/ichi-theinterface" rel="noreferrer" target="_blank">
+            <img className="img-responsive" src={github} alt="logo"/>
+	         </a>
+           </li>
         </ul>
       </div>
 
-      <div class="col-6 col-s-9">
-        <h1>The City</h1>
-        <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
-      </div>
-
-      <div class="col-3 col-s-12">
-        <div class="aside">
-          <h2>What?</h2>
-          <p>Chania is a city on the island of Crete.</p>
-          <h2>Where?</h2>
-          <p>Crete is a Greek island in the Mediterranean Sea.</p>
-          <h2>How?</h2>
-          <p>You can reach Chania airport from all over Europe.</p>
-        </div>
+      <div class="col-6 col-s-9 main">
+        <h1 className="NameinGreeting">What's up - I'm Ichiro!</h1>
+        <p>
+          Stay hungy and curious about the possibility of technology.<br/>
+          What I love about IT is that more I learn about it,<br/>
+          more things I find that I do not know!! 
+      </p>
       </div>
     </div>
 )
