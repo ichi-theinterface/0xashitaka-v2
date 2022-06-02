f23f1d9c9b9025d7a9b74e51729bbff75af7ec2d
Ichiro Yamasaki <ichi.theinterface@gmail.com>
2022-06-01 17:43:23 -0700

    commit on 06-01

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+import { Link } from "react-router-dom";
+
+const Header = () => (
+    <div class="header">
+      <h1>Chania</h1>
+     </div>
+)
+
+export default Header;
