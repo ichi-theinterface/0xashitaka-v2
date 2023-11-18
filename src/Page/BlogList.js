dc134518815d95ab7f57be06961591d080a02eb4
Eiichiro Yamasaki <ichiro@Eiichiros-MacBook-Pro.local>
2023-11-18 11:30:14 -0800

    added PostCard componet for BlogList page

--- src/Page/BlogList.js
+++ src/Page/BlogList.js
@@ -1,26 +1,13 @@
 import './Blog.css';
-import Header from '../component/Header'
-import { Link } from "react-router-dom";
-import blogs from "../blogs.json"
+import Header from '../component/Header';
+import PostCard from '../component/PostCard';
 
 function BlogList() {
-  //const [blogs, setBlogs] = useState([]);
-
-  //useEffect(() => {
-  //  fetch('YOUR_API_ENDPOINT_HERE') // Replace with your API endpoint
-  //    .then(response => response.json())
-  //    .then(data => setBlogs(data));
-  //}, []);
-
   return (
     <div className='canvas'>
       <Header />
       <div className='blog-post-frame'>
-        {blogs.map(blog => (
-          <div className='blog-post-cards' key={blog.id}>
-            <Link to={`/blog/${blog.id}`}>{blog.title}</Link>
-          </div>
-        ))}
+        <PostCard />
       </div>
     </div>
   );
