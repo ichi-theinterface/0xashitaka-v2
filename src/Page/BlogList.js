91608e815520bcce71f53f6249b6b2474ed0dd13
Eiichiro Yamasaki <ichiro@Eiichiros-MacBook-Pro.local>
2023-11-21 21:53:18 -0800

    added header to the BlogList page

--- src/Page/BlogList.js
+++ src/Page/BlogList.js
@@ -6,6 +6,9 @@ function BlogList() {
   return (
     <div className='canvas'>
       <Header />
+      <div className='BlogList-Header-Frame'>
+        <h1 className='BlogList-Header'>My Engineering Blog</h1>
+      </div>
       <div className='blog-post-frame'>
         <PostCard />
       </div>
